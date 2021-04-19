9484bdd65633c1e89952b84757ed89923d156389
fabiopisa <pisanellifabio@gmail.com>
2021-04-19 16:00:47 +0200

    commit esercizio finito

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,16 @@
 // creaiamo la situazione di partenza per scrivere il nostro codice
 $(document).ready(function(){
-// faccio le referenze
-  var listBurger = $("hamburger-menu");
-  var menuBurger = $("header-right > a");
-// do il comando al click
+  // faccio le referenze
+  var listBurger = $(".hamburger-menu");
+  var menuBurger = $(".header-right > a");
+  var closeMenu = $(".close");
+  // do il comando al click
   menuBurger.click(function(){
    listBurger.show();
+  });
+  // do il comando di chiusura con il click sulla x del menu
+  closeMenu.click(function(){
+   listBurger.hide();
   });
 
 });
